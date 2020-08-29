4f5f4a9c453bf361150a65a8a6fddfbb1ee0c447
Josh Kall <joshuakall@utexas.edu>
2020-08-29 18:08:35 -0500

    Add Boost dependency to vera++ tool.

--- scripts/includes.py
+++ scripts/includes.py
@@ -1,11 +1,12 @@
-# Contains useful functions.
+# Contains useful functions and constants.
 
 from pathlib import Path
 import platform
 import json
 
+json_file_path = Path(__file__).parent / "../bb_embedded.json"
+
 def set_os():
-    json_file_path = Path(__file__).parent / "../bb_embedded.json"
     bb_embedded = open(json_file_path, "r")
     embedded_json = json.load(bb_embedded)
     bb_embedded.close()
@@ -15,3 +16,13 @@ def set_os():
     json.dump(embedded_json, bb_embedded, indent=4)
     bb_embedded.close()
 
+def install_tools():
+    bb_embedded = open(json_file_path, "r")
+    embedded_json = json.load(bb_embedded)
+    tools = embedded_json["tools"]
+    for tool in tools:
+        install_tool(tool)
+    bb_embedded.close()
+
+def install_tool(tool_config):
+    print("Installing " + tool_config["name"])
--- scripts/install_bbe.py
+++ scripts/install_bbe.py
@@ -1,3 +1,12 @@
+import os
 import includes
 
+print("Welcome to Bottle Buddy Embedded!!!")
+
 includes.set_os()
+
+os.system("mkdir downloads")
+
+includes.install_tools()
+
+os.system("rm -rf downloads")
