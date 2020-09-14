f2f012ae33cc0e3af5e2c69dd7ce03104a2f5952
JoshKall21 <joshuakall@verizon.net>
2020-09-13 19:53:19 -0500

    Fix lint errors.

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,22 @@
 #include <Arduino.h>
 
-int ledpin = 2;
+constexpr int serialSpeed = 115200;
+constexpr int delayTime = 1000;
+constexpr int ledPin = 2;
 
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
-  pinMode(ledpin, OUTPUT);
-  Serial.begin(115200);
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(serialSpeed, SERIAL_8N1); // NOLINT(fuchsia-default-arguments-calls)
 }
 
 void loop() {
-  digitalWrite(ledpin, HIGH);   // turn the LED on (HIGH is the voltage level)
-  Serial.print(1);
+  digitalWrite(ledPin, HIGH);   // turn the LED on (HIGH is the voltage level)
+  Serial.print(1);              // NOLINT(fuchsia-default-arguments-calls)
   Serial.print("\t");
-  delay(1000);                       // wait for a second
-  digitalWrite(ledpin, LOW);    // turn the LED off by making the voltage LOW
-  Serial.print(0);
-  Serial.print("\t");
-  delay(1000);                       // wait for a second
+  delay(delayTime);                       // wait for a second
+  digitalWrite(ledPin, LOW);    // turn the LED off by making the voltage LOW
+  Serial.print(0);              // NOLINT(fuchsia-default-arguments-calls)
+  Serial.print("\t");           
+  delay(delayTime);                       // wait for a second
 }
