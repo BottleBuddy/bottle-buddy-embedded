deceab65e44bc14becd2f84d1f0570a272f96f81
unknown <thezanefreeman@utexas.edu>
2020-11-27 17:21:59 -0600

    Create general setup branch with pin and serial initialization.

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,40 @@
-#include <Arduino.h>
+/*************************************************************
+ This is the official test software for the bottle buddy hardware
+ Written by Zane Freeman and Jason Siegel
+ *************************************************************
+  Note: This requires the following libraries:
+          Adafruit_VL53L0X Library
+          AdafruitBLE Library
+          Arduino_LSM9DS1 Library
+ *************************************************************
+ To run the BLE functions, download LightBlue App on Google Play/AppStore
+ *************************************************************/
 
-constexpr int serialSpeed = 115200;
-constexpr int delayTime = 1000;
-constexpr int ledPin = 2;
+/*HEADER FILES FOR SOFTWARE*/
+#include <Arduino.h>
+/**************************/
+
+/*Global Variables*/
+const int GREEN_LED_PIN = 4;
+const int RED_LED_PIN = 3;
+const int BLUE_LED_PIN = 2;
+const int BUTTON_PIN = 5;
+const int BAUD_RATE = 115200;
+/******************/
 
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(ledPin, OUTPUT);
-  Serial.begin(serialSpeed, SERIAL_8N1); // NOLINT(fuchsia-default-arguments-calls)
+  /*Pin Initializations*/
+  pinMode(BLUE_LED_PIN, OUTPUT);
+  pinMode(RED_LED_PIN, OUTPUT);
+  pinMode(GREEN_LED_PIN, OUTPUT);
+  pinMode(BUTTON_PIN, INPUT);
+  /*********************/
+
+  /*Serial Initialization for Testing*/
+  Serial.begin(BAUD_RATE);
+  while (! Serial){delay(1);}
+  Serial.println("Bottle Buddy Test Program");
+  /**********************************/
 }
 
-void loop() {
-  digitalWrite(ledPin, HIGH);   // turn the LED on (HIGH is the voltage level)
-  Serial.print(1);              // NOLINT(fuchsia-default-arguments-calls)
-  Serial.print("\t");
-  delay(delayTime);                       // wait for a second
-  digitalWrite(ledPin, LOW);    // turn the LED off by making the voltage LOW
-  Serial.print(0);              // NOLINT(fuchsia-default-arguments-calls)
-  Serial.print("\t");           
-  delay(delayTime);                       // wait for a second
-}
+void loop() {}
