5d07ba36d9a9f933f24e04361cffa20f3c914b8c
JoshKall21 <joshuakall@verizon.net>
2021-01-25 22:31:20 -0600

    Added documentation to pipeline architecture.

--- include/Pipeline/pipe.h
+++ include/Pipeline/pipe.h
@@ -9,14 +9,30 @@
 
 namespace BottleBuddy { namespace Embedded { namespace Pipeline {
 
+    /**
+     * @brief Transports sensor data.
+     * 
+     * This is a general class used to bi-directionally transport sensor data across the Bottle Buddy embedded system.
+     */
     class Pipe {
     public:
-        Pipe();
         Pipe(Location location);
 
+        /**
+         * @brief Sends sensor data.
+         * 
+         * Use this function when a sensor reads new data that will be used across the Bottle Buddy embedded system.
+         */
         template<typename T>
         void sendPayload(T payload);
     private:
+        Pipe();
+
+        /**
+         * @brief Pipe's sensor location.
+         * 
+         * Used by the Router class to determine what to do with the sensor data that comes down this pipe.
+         */
         Location location;
     };
 
