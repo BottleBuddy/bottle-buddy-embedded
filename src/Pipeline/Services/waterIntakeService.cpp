11ed63091dacfd9c8d29a1b8663fe72662a821cd
JoshKall21 <joshuakall@verizon.net>
2021-02-05 18:19:47 -0600

    Created BLE characteristics for WaterIntakeService.

--- src/Pipeline/Services/waterIntakeService.cpp
+++ src/Pipeline/Services/waterIntakeService.cpp
@@ -5,6 +5,10 @@
 #include "Pipeline/Services/waterIntakeService.h"
 
 BottleBuddy::Embedded::Pipeline::Services::WaterIntakeService::WaterIntakeService(const char* uid) : Service(uid) {
+    createCharacteristic(std::string("waterIntake"), BLERead);
+    createCharacteristic(std::string("time"), BLEWrite);
+    createCharacteristic(std::string("received"), BLERead | BLEWrite);
+
     BottleBuddy::Embedded::Pipeline::Router::subscribe(BottleBuddy::Embedded::Pipeline::Location::ToF, this);
 }
 
