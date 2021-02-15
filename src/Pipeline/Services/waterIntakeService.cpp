581a6fd7ffce6ed8c4da3140e8c014cae46a5d74
JoshKall21 <joshuakall@verizon.net>
2021-02-15 17:31:20 -0600

    Configure WaterIntakeService for BLE integration.

--- src/Pipeline/Services/waterIntakeService.cpp
+++ src/Pipeline/Services/waterIntakeService.cpp
@@ -5,9 +5,11 @@
 #include "Pipeline/Services/waterIntakeService.h"
 
 BottleBuddy::Embedded::Pipeline::Services::WaterIntakeService::WaterIntakeService(const char* uid) : Service(uid) {
-    createCharacteristic(std::string("waterIntake"), BLERead);
-    createCharacteristic(std::string("time"), BLEWrite);
-    createCharacteristic(std::string("received"), BLERead | BLEWrite);
+    createCharacteristic(std::string("water_intake"), BLERead | BLEIndicate);
+    createCharacteristic(std::string("time"), BLEWrite | BLENotify);
+    createCharacteristic(std::string("received_id"), BLEWrite | BLENotify);
+
+    BLE.setAdvertisedServiceUuid(uid);
 
     BottleBuddy::Embedded::Pipeline::Router::subscribe(BottleBuddy::Embedded::Pipeline::Location::ToF, this);
     BottleBuddy::Embedded::Pipeline::Router::subscribe(BottleBuddy::Embedded::Pipeline::Location::ACCELEROMETER, this);
