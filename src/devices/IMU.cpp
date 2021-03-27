044995bcbea4b0c8363bcb0d6b9ebacb1458eb96
JoshKall21 <joshuakall@verizon.net>
2021-03-27 18:33:47 -0500

    Add functions to read IMU gyroscope and magnetometer.

--- src/devices/IMU.cpp
+++ src/devices/IMU.cpp
@@ -17,58 +17,22 @@ void read_accelerometer(float& x, float& y, float& z) {
     z = z_dim;
 }
 
-/* THE FUNCTION BELOW DOES NOT WORK! But it's on the right track :)
- * ORIENTATION CHECKER
- * Description: This returns an integer value corresponding to 
- * the orientation of the cap. 
- *    1 = right side up
- *    2 = up side down
- *    3 = tilted left
- *    4 = left
- *    5 = tilted right 
- *    6 = right
- * Input: None
- * Output: Integer value oreintation
-*/
-int orientation(){
-    float IMU_DELTA = .1;
-  float x, y, z;
-  if (IMU.accelerationAvailable())
-  {
-    IMU.readAcceleration(x, y, z);
- 
-    if(y >= -IMU_DELTA && y <= 0)
-          //Serial.println("flat");
-          return 1;
-    if(y <= IMU_DELTA && y > 0)
-          //Serial.println("upside down"); 
-          return 2;   
-    if(y > IMU_DELTA && y < 1 - IMU_DELTA)
-          //Serial.println("tilted to the left");
-          return 3;
-    if(y >= 1 - IMU_DELTA)
-          //Serial.println("left");
-          return 4;
-    if(y < -IMU_DELTA && y > IMU_DELTA - 1)
-          //Serial.println("tilted to the right");
-          return 5;
-    if(y <= IMU_DELTA - 1)
-          //Serial.println("right");
-          return 6;
-  }
+void read_gyroscope(float& x, float& y, float& z) {
+    float x_dim, y_dim, z_dim;
+    while(!IMU.gyroscopeAvailable())
+        ;
+    IMU.readGyroscope(x_dim, y_dim, z_dim);
+    x = x_dim;
+    y = y_dim;
+    z = z_dim; 
 }
 
-//This shows you how to read the gyroscope
-int gyroscope_example(){
-      float x, y, z;
-
-  if (IMU.gyroscopeAvailable()) {
-        IMU.readGyroscope(x, y, z);
-
-        Serial.print(x);
-        Serial.print('\t');
-        Serial.print(y);
-        Serial.print('\t');
-        Serial.println(z);
-  }
+void read_magnetometer(float& x, float& y, float& z) {
+    float x_dim, y_dim, z_dim;
+    while(!IMU.magneticFieldAvailable())
+        ;
+    IMU.readMagneticField(x_dim, y_dim, z_dim);
+    x = x_dim;
+    y = y_dim;
+    z = z_dim; 
 }
