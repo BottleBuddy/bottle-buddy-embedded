c7cde5d627acda65e3f2807be79b46b58c4e9dfb
JoshKall21 <joshuakall@verizon.net>
2021-03-28 02:49:14 -0500

    Remove cleaning service from this branch.

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,6 @@
 
 #include <Arduino.h>
 #include "Pipeline/Services/waterIntakeService.h"
-#include "Pipeline/Services/cleaningService.h"
 #include "Pipeline/pipeFactory.h"
 #include "devices/ToF.h"
 #include "devices/IMU.h"
@@ -20,7 +19,6 @@ BottleBuddy::Embedded::Pipeline::Pipe *gyroscopePipe;
 BottleBuddy::Embedded::Pipeline::Pipe *magnetometerPipe;
 
 BottleBuddy::Embedded::Pipeline::Service *waterIntakeService;
-BottleBuddy::Embedded::Pipeline::Service *cleaningService;
 
 /**
  * @brief Serial speed
@@ -62,7 +60,6 @@ void setup() {
   }
 
   waterIntakeService = new BottleBuddy::Embedded::Pipeline::Services::WaterIntakeService("19B10010-E8F2-537E-4F6C-D104768A1214");
-  cleaningService = new BottleBuddy::Embedded::Pipeline::Services::CleaningService("19B10020-E8F2-537E-4F6C-D104768A1214");
   int advertising_success = advertise_ble();
 
   waterLevelPipe = BottleBuddy::Embedded::Pipeline::PipeFactory::producePipe(BottleBuddy::Embedded::Pipeline::Location::ToF);
