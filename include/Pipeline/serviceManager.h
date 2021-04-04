80bff39ef8f9ada8957b033662923e074c22ab7d
JoshKall21 <joshuakall@verizon.net>
2021-04-04 00:50:13 -0500

    Added comments to service manager.

--- include/Pipeline/serviceManager.h
+++ include/Pipeline/serviceManager.h
@@ -1,9 +1,5 @@
 /**
  * @file serviceManager.h
- * 
- * @brief Manages all instantiated services.
- * 
- * Executes important lifecycle events for services such as looping, connecting, and disconnecting.
  */
 
 #pragma once
@@ -14,19 +10,48 @@
 
 namespace BottleBuddy { namespace Embedded { namespace Pipeline { 
 
+    /**
+     * @brief Manages all instantiated services.
+     * 
+     * Executes important lifecycle events for services such as looping, connecting, and disconnecting.
+     */
     class ServiceManager {
     public:
         ServiceManager();
 
         ~ServiceManager();
 
+        /**
+         * @brief Adds a service to the list of services the service manager handles.
+         * 
+         * The provided service will now be connected, disconnected, and looped.
+         */
         void addService(Service* service);
 
+        /**
+         * @brief Calls the virtual connect function on all managed services.
+         * 
+         * Call this function when the BLE device connects to a central device.
+         */
         void connectedBLE();
+
+        /**
+         * @brief Calls the virtual disconnect function on all managed services.
+         * 
+         * Call this function when the BLE device disconnects from a central device.
+         */
         void disconnectedBLE();
 
+        /**
+         * @brief Calls the virtual loop function on all managed services.
+         * 
+         * Call this function once per main loop.
+         */
         void loopServices();
     private:
+        /**
+         * @brief The list of managed services.
+         */
         std::vector<Service*> services;
     };
 
