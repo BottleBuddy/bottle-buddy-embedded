ebe07c054460a2959ae15d3d1eea7facb9c27b29
JoshKall21 <joshuakall@verizon.net>
2021-04-06 21:02:22 -0500

    Changed main file to match develop branch.

--- src/main.cpp
+++ src/main.cpp
@@ -22,20 +22,12 @@ BottleBuddy::Embedded::Pipeline::Pipe *fsrPipe;
 
 BottleBuddy::Embedded::Pipeline::ServiceManager *serviceManager;
 
-const int GREEN_LED_PIN = 4;
-const int RED_LED_PIN = 3;
-const int BLUE_LED_PIN = 2;
-
 /**
  * @brief Setup loop.
  * 
  * Makes necessary initializations for system to be able to run.
  */
 void setup() {
-  pinMode(BLUE_LED_PIN, OUTPUT);
-  pinMode(RED_LED_PIN, OUTPUT);
-  pinMode(GREEN_LED_PIN, OUTPUT);
-  pinMode(LED_BUILTIN, OUTPUT);
 
   if(tof_sensor_setup() == -1) {
     Serial.println("Failed to initialize VL53L0X!");
