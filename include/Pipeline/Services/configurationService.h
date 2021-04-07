568881860db265af75c47c89ce623f86a96671e4
JoshKall21 <joshuakall@verizon.net>
2021-04-06 19:27:05 -0500

    Created configuration service.

---
+++ include/Pipeline/Services/configurationService.h
@@ -0,0 +1,23 @@
+/**
+ * @file configurationService.h
+ */
+
+#pragma once
+
+#include "Pipeline/service.h"
+
+namespace BottleBuddy { namespace Embedded { namespace Pipeline { namespace Services {
+
+    class ConfigurationService : public Service {
+    public:
+        ConfigurationService(const char* uid);
+
+        void connect();
+        void disconnect();
+
+        void loop();
+        void receive(Package* package);
+    private:
+    };
+
+}}}}
---
+++ src/Pipeline/Services/configurationService.cpp
@@ -0,0 +1,25 @@
+/**
+ * @file configurationService.cpp
+ */
+
+#include "Pipeline/Services/configurationService.h"
+
+BottleBuddy::Embedded::Pipeline::Services::ConfigurationService::ConfigurationService(const char* uid) : Service(uid) {
+
+}
+
+void BottleBuddy::Embedded::Pipeline::Services::ConfigurationService::connect() {
+
+}
+
+void BottleBuddy::Embedded::Pipeline::Services::ConfigurationService::disconnect() {
+
+}
+
+void BottleBuddy::Embedded::Pipeline::Services::ConfigurationService::loop() {
+
+}
+
+void BottleBuddy::Embedded::Pipeline::Services::ConfigurationService::receive(Package* package) {
+    
+}
